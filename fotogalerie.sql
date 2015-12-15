18397e9bfec99a73a00a4c35ea39f69956d2413a
Jan Kuril <jankuril@seznam.cz>
2015-12-15 21:11:16 +0100

    DB

--- fotogalerie.sql
+++ fotogalerie.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Počítač: 127.0.0.1
--- Vytvořeno: Ned 29. lis 2015, 15:19
+-- Vytvořeno: Úte 15. pro 2015, 21:10
 -- Verze serveru: 5.6.26
 -- Verze PHP: 5.6.12
 
@@ -19,6 +19,19 @@ SET time_zone = "+00:00";
 --
 -- Databáze: `fotogalerie`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `akce`
+--
+
+CREATE TABLE IF NOT EXISTS `akce` (
+  `id_akce` int(11) NOT NULL,
+  `nazev` varchar(80) COLLATE utf8_czech_ci NOT NULL,
+  `popis` text COLLATE utf8_czech_ci NOT NULL,
+  `plati` varchar(80) COLLATE utf8_czech_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 -- --------------------------------------------------------
 
@@ -53,6 +66,30 @@ INSERT INTO `fotky` (`id_fotky`, `miniatura`, `plna`, `zobrazit`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Struktura tabulky `intro`
+--
+
+CREATE TABLE IF NOT EXISTS `intro` (
+  `id_intro` int(11) NOT NULL,
+  `miniatura` varchar(80) COLLATE utf8_czech_ci NOT NULL,
+  `plna` varchar(80) COLLATE utf8_czech_ci NOT NULL,
+  `zobrazit` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+--
+-- Vypisuji data pro tabulku `intro`
+--
+
+INSERT INTO `intro` (`id_intro`, `miniatura`, `plna`, `zobrazit`) VALUES
+(1, 'media/img/intro/nahledy/1.jpg', 'media/img/intro/fotky/1.jpg', 1),
+(2, 'media/img/intro/nahledy/2.jpg', 'media/img/intro/fotky/2.jpg', 1),
+(3, 'media/img/intro/nahledy/3.jpg', 'media/img/intro/fotky/3.jpg', 1),
+(4, 'media/img/intro/nahledy/4.jpg', 'media/img/intro/fotky/4.jpg', 1),
+(5, 'media/img/intro/nahledy/5.jpg', 'media/img/intro/fotky/5.jpg', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabulky `kategorie_masazi`
 --
 
@@ -61,7 +98,7 @@ CREATE TABLE IF NOT EXISTS `kategorie_masazi` (
   `nazev` varchar(40) COLLATE utf8_czech_ci NOT NULL,
   `popis` text COLLATE utf8_czech_ci NOT NULL,
   `obrazek` varchar(80) COLLATE utf8_czech_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 --
 -- Vypisuji data pro tabulku `kategorie_masazi`
@@ -78,6 +115,26 @@ INSERT INTO `kategorie_masazi` (`id_kategorie`, `nazev`, `popis`, `obrazek`) VAL
 -- --------------------------------------------------------
 
 --
+-- Struktura tabulky `kategorie_oleju`
+--
+
+CREATE TABLE IF NOT EXISTS `kategorie_oleju` (
+  `id_kategorie` int(11) NOT NULL,
+  `nazev` varchar(80) COLLATE utf8_czech_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+--
+-- Vypisuji data pro tabulku `kategorie_oleju`
+--
+
+INSERT INTO `kategorie_oleju` (`id_kategorie`, `nazev`) VALUES
+(1, 'DETOXIKAČNÍ OLEJE'),
+(2, 'HARMONIZAČNÍ OLEJE'),
+(3, 'REJUVENIZAČNÍ OLEJE');
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabulky `masaze`
 --
 
@@ -89,7 +146,7 @@ CREATE TABLE IF NOT EXISTS `masaze` (
   `cas` int(11) NOT NULL,
   `cena` int(11) NOT NULL,
   `id_kategorie` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 --
 -- Vypisuji data pro tabulku `masaze`
@@ -103,6 +160,31 @@ INSERT INTO `masaze` (`id_masaze`, `nazev`, `popis`, `obrazek`, `cas`, `cena`, `
 (5, 'fewfew', '<p>fewfewf</p>\r\n', 'media/img/masaze/5.jpg', 5225, 2525, 6),
 (6, 'cewc', '<p>ecewcec</p>\r\n', 'media/img/masaze/6.jpg', 254, 25, 1),
 (7, 'ds', '<p>ds ds </p>\r\n', 'media/img/masaze/7.jpg', 28, 28, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `oleje`
+--
+
+CREATE TABLE IF NOT EXISTS `oleje` (
+  `id_oleje` int(11) NOT NULL,
+  `nazev` varchar(80) COLLATE utf8_czech_ci NOT NULL,
+  `popis` text COLLATE utf8_czech_ci NOT NULL,
+  `cena` int(11) NOT NULL,
+  `id_kategorie` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+--
+-- Vypisuji data pro tabulku `oleje`
+--
+
+INSERT INTO `oleje` (`id_oleje`, `nazev`, `popis`, `cena`, `id_kategorie`) VALUES
+(1, 'ROZMARÝNOVÝ OLEJ', '<p>Silně stimulující a harmonizující silice, nervová bolest hlavy, migréna, únava, antiseptické účinky, podporuje žaludeční činnost, antispazmatickém, svalové bolesti, nedokrevnost končetin, celulitida, stimuluje srdeční činnost, diuretikum, brání vzniku jizev, normalizuje krevní tlak, tonizační, napomáhá regeneraci vlasové pokožky.</p>\r\n', 100, 1),
+(2, 'LEVANDULOVÝ OLEJ', '<p>Uklidňuje, pomáhá při nervovém vypětí, bolesti hlavy, analgetikum, dýchací problémy, bronchitida, menstruační bolesti, revmatické bolesti, svalové bolesti, repelent, snižuje tlak, hodí se na všechny typy pleti, regeneruje kožní buňky, antiseptikum, normalizuje mazotok, má hojivé účinky.</p>\r\n', 100, 2),
+(3, 'SANTALOVÝ OLEJ', '<p>Působí na nervový systém, antidepresivum, afrodiziakum, sedativum, nespavost, psychosomatické problémy, diuretikum, katar, kašel, nachlazení, bolesti v krku, stimuluje trávení, nevolnost, tonikum, akné, mastná pleť, zvláčňuje suchou a dehydratovanou pleť.</p>\r\n', 100, 3),
+(4, 'MEGA SUPER OLEJ', '<p>FAKT SUPER OLEJ PRO VŠECHNY</p>\r\n', 123, 3),
+(5, 'Tak toto mosíš mět', '<p>Tak taková olej mosíš zkusit</p>\r\n', 159, 2);
 
 -- --------------------------------------------------------
 
@@ -128,15 +210,33 @@ INSERT INTO `user` (`id`, `login`, `password`) VALUES
 --
 
 --
+-- Klíče pro tabulku `akce`
+--
+ALTER TABLE `akce`
+  ADD PRIMARY KEY (`id_akce`);
+
+--
 -- Klíče pro tabulku `fotky`
 --
 ALTER TABLE `fotky`
   ADD PRIMARY KEY (`id_fotky`);
 
 --
+-- Klíče pro tabulku `intro`
+--
+ALTER TABLE `intro`
+  ADD PRIMARY KEY (`id_intro`);
+
+--
 -- Klíče pro tabulku `kategorie_masazi`
 --
 ALTER TABLE `kategorie_masazi`
+  ADD PRIMARY KEY (`id_kategorie`);
+
+--
+-- Klíče pro tabulku `kategorie_oleju`
+--
+ALTER TABLE `kategorie_oleju`
   ADD PRIMARY KEY (`id_kategorie`);
 
 --
@@ -146,24 +246,51 @@ ALTER TABLE `masaze`
   ADD PRIMARY KEY (`id_masaze`);
 
 --
+-- Klíče pro tabulku `oleje`
+--
+ALTER TABLE `oleje`
+  ADD PRIMARY KEY (`id_oleje`),
+  ADD KEY `id_kategorie` (`id_kategorie`);
+
+--
 -- AUTO_INCREMENT pro tabulky
 --
 
+--
+-- AUTO_INCREMENT pro tabulku `akce`
+--
+ALTER TABLE `akce`
+  MODIFY `id_akce` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pro tabulku `fotky`
 --
 ALTER TABLE `fotky`
   MODIFY `id_fotky` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
 --
+-- AUTO_INCREMENT pro tabulku `intro`
+--
+ALTER TABLE `intro`
+  MODIFY `id_intro` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
 -- AUTO_INCREMENT pro tabulku `kategorie_masazi`
 --
 ALTER TABLE `kategorie_masazi`
-  MODIFY `id_kategorie` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `id_kategorie` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT pro tabulku `kategorie_oleju`
+--
+ALTER TABLE `kategorie_oleju`
+  MODIFY `id_kategorie` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT pro tabulku `masaze`
 --
 ALTER TABLE `masaze`
-  MODIFY `id_masaze` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+  MODIFY `id_masaze` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT pro tabulku `oleje`
+--
+ALTER TABLE `oleje`
+  MODIFY `id_oleje` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
