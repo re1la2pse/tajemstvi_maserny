ea61ff89fd92499cf7711e2086bdac85e7a73648
re1la2pse <mokrusa.p@gmail.com>
2016-04-19 20:31:48 +0200

    jquery-ui, dialog reklama

--- media/js/main.js
+++ media/js/main.js
@@ -3,5 +3,9 @@ $(function (){
     $('.carousel').carousel({
         interval: 3500, //changes the speed
         pause: "none"
-    })    
+    })
+
+    $("#dialogReklama span").click(function() {
+       $(this).parents('div').hide();
+    });
 });
